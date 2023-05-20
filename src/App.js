948951de2efc191914fc431b1048d7a89396ca0b
Facundo VARAS <102789450+cundovar@users.noreply.github.com>
2023-05-20 21:43:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Home from './page/Home';
+
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+
+    <Routes>
+             
+
+            <Route path="/" element= {<Home/> } />
+             
+        </Routes> 
+    </Router>
+  );
+}
+
+export default App;
